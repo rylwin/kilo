821a91616abc6f0c548b4edcf2d575bc0c5ce382
Ryan Winograd <ryan@thewinograds.com>
2017-04-16 18:37:37 -0500

    Add single-line-comment highlighting

--- kilo.c
+++ kilo.c
@@ -37,6 +37,7 @@ enum editorKey {
 enum editorHighlight {
   HL_NORMAL = 0,
   HL_STRING,
+  HL_COMMENT,
   HL_NUMBER,
   HL_MATCH
 };
@@ -47,6 +48,7 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -83,6 +85,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMERS | HL_HIGHLIGHT_STRINGS
   },
 };
@@ -417,10 +420,20 @@ void editorUpdateSyntax(erow *row) {
   int prev_sep = 1;
   int in_string = 0;
 
+  char *scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int i = 0;
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i < 0) ? HL_NORMAL : row->hl[i - 1];
+
+    if (scs_len && !in_string) {
+      if (strncmp(&row->render[i], scs, scs_len) == 0) {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+        break;
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
       if (in_string) {
@@ -461,6 +474,7 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch(hl) {
+    case HL_COMMENT: return 36;
     case HL_STRING: return 35;
     case HL_NUMBER: return 31;
     case HL_MATCH: return 34;
